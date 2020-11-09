c4ea88cfa2e60af5793fd5c9ce9f0734066ab050
cn_randy <randy@twohalls.com>
2020-11-09 17:43:19 -0500

    Update book

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 /**
  * Class BooksController
@@ -18,4 +21,57 @@ class BooksController
     {
             return Book::all();  
     }
+
+    public function show(Request $request, $id)
+    {
+        try {
+            return Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ],
+            ],  Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+     * POST /books
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+     public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+
+        return response()->json([
+            'created' => true], 
+            Response::HTTP_CREATED, [
+            'Location' => route('books.show', ['id' => $book->id])
+            ]);
+    }
+
+        /**
+     * PUT /books/{id}
+     *
+     * @param Request $request
+    * @param $id
+    * @return mixed
+    */
+    public function update(Request $request, $id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                'message' => 'Book not found'
+            ]
+            ], 404);
+        }
+        
+        $book->fill($request->all());
+        $book->save();
+        return $book;
+        }
 }
